40dc144fb8e706e5b955cf76da344aeb42388853
Jeremy Park <seungilpark88@gmail.com>
2021-01-30 10:59:05 -0800

    Created interface Repositories for claim

---
+++ ClaimManager.Application/Interfaces/Repositories/Claims/IClaimItemRepository.cs
@@ -0,0 +1,24 @@
+﻿using ClaimManager.Domain.Entities.Claims;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClaimManager.Application.Interfaces.Repositories.Claims
+{
+    public interface IClaimItemRepository
+    {
+        IQueryable<ClaimItem> ClaimItems { get; }
+
+        Task<List<ClaimItem>> GetListAsync();
+
+        Task<ClaimItem> GetByIdAsync(int itemId);
+
+        Task<int> InsertAsync(ClaimItem claimItem);
+
+        Task UpdateAsync(ClaimItem claimItem);
+
+        Task DeleteAsync(ClaimItem claimItem);
+    }
+}
