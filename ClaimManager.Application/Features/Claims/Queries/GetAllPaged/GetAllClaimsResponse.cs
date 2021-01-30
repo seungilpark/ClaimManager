68667502d65a13884f95a96771c5bb0e37107f87
Jeremy Park <seungilpark88@gmail.com>
2021-01-30 11:38:30 -0800

    Get All Claims Query/ response and handler

---
+++ ClaimManager.Application/Features/Claims/Queries/GetAllPaged/GetAllClaimsResponse.cs
@@ -0,0 +1,26 @@
+﻿using ClaimManager.Domain.Entities.Claims;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClaimManager.Application.Features.Claims.Queries.GetAllPaged
+{
+    public class GetAllClaimsResponse
+    {
+        public string Title { get; set; }
+        public string RequesterId { get; set; }
+        public string ApproverId { get; set; }
+        public DateTime SubmitDate { get; set; }
+        public DateTime ApprovalDate { get; set; }
+        public DateTime ProcessedDate { get; set; }
+        public decimal TotalAmount { get; set; }
+        public ClaimStatus Status { get; set; } 
+        public string RequesterComments { get; set; }
+        public string ApproverComments { get; set; }
+        public string FinanceComments { get; set; }
+        public ICollection<ClaimItem> ClaimItems { get; set; }
+
+    }
+}
